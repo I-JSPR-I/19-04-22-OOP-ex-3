0421033a5a8b4eb5e83a32130bfdec4f0da34520
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-04-25 08:59:35 +0200

    Add project files.

---
+++ 19-04-22 OOP ex 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _19_04_22_OOP_ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                for(int i = 0; i < 10;)
+                {
+                    Console.Clear();
+                    string time = DateTime.Now.ToString("h:mm:ss tt");
+                    Console.WriteLine(time);
+                    System.Threading.Thread.Sleep(1000);
+                   
+                }
+                
+            }
+        }
+    }
+}
